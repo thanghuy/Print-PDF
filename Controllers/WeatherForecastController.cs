071307d2f72fcf026ee998c28ceb2514e9a3ba2e
unknown <huythang0903@gmail.com>
2021-11-24 15:36:05 +0700

    u

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PrintPDF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wkhtmltopdf.NetCore;
+
+namespace PrintPDF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        readonly IGeneratePdf _generatePdf;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IGeneratePdf generatePdf)
+        {
+            _logger = logger;
+            _generatePdf = generatePdf;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = new Test
+            {
+                Text = "This is a test",
+                Number = 123456
+            };
+
+            return await _generatePdf.GetPdf("Views/Test.cshtml", data);
+        }
+    }
+}

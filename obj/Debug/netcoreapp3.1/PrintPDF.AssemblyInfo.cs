62fc3210ad0af13e3295ad14c2cd22b9f5f4a95b
unknown <huythang0903@gmail.com>
2021-11-24 16:13:09 +0700

    a

--- obj/Debug/netcoreapp3.1/PrintPDF.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/PrintPDF.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("e4859cd9-f4b4-45e5-a115-30fe34e724f7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintPDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
